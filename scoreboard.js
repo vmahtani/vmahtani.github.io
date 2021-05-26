0f5230f239b79d0b96c3d1c6b9a68fba81155efa
unknown <vmahtani1@gmail.com>
2021-05-25 20:01:01 -0400

    show hide

---
+++ scoreboard.js
@@ -0,0 +1,15 @@
+    function toggleteamranks(callerid) {
+        var togglediv = document.getElementById(callerid+'div');
+        var togglelabel = document.getElementById(callerid);
+        if(togglediv.style.display=='block')
+        {
+            togglediv.style.display='none';
+            togglelabel.innerText = "+" + togglelabel.innerText.substring(1);
+        }
+        else
+        {
+            togglediv.style.display='block';
+            togglelabel.innerText = "-" + togglelabel.innerText.substring(1);
+        }    
+    }
+
