778b9cffbd7619a4f68ad85bc143fbec4f63ce78
unknown <vmahtani1@gmail.com>
2022-01-24 15:00:01 -0500

    changes

--- nba/files/sng.js
+++ nba/files/sng.js
@@ -71,43 +71,42 @@
     }
 
     var lineup={ 
-        "PG1":["",0], 
-        "PG2":["",0], 
-        "SG1":["",0], 
-        "SG2":["",0], 
-        "SF1":["",0], 
-        "SF2":["",0], 
-        "PF1":["",0], 
-        "PF2":["",0], 
-        "C":  ["",0] 
-       };
-   
-       function updatelineup(pos, name, salary) {
-           if(pos!="C") {
-               pos1 = pos + "1";
-               pos2 = pos + "2";
-               console.log(lineup[pos1]);
-               if(lineup[pos1][0]=="")
-                   lineup[pos1] = [name,salary];
-               else
-                   lineup[pos2] = [name,salary];
-           }
-           else
-               lineup[pos] = [name,salary];
-   
-           console.log(lineup);
-           writelineupdiv();
-       }
-       function writelineupdiv()
-       {
-           var dfslineuptext = "";
-           var totalsalary = 0; 
-           for (var key in lineup){
-               dfslineuptext += "<h3>" + key + " " + lineup[key][0] + " " + lineup[key][1] + "</h3>" ;
-               totalsalary += parseInt(lineup[key][1]);
-               //console.log( key, dict[key] );
-           }
-           dfsdiv = document.getElementById('dfslineupdiv');
-           dfsdiv.innerHTML = dfslineuptext + "<h3>" + totalsalary + "</h3>";
-       }
-       
+     "PG1":["",0], 
+     "PG2":["",0], 
+     "SG1":["",0], 
+     "SG2":["",0], 
+     "SF1":["",0], 
+     "SF2":["",0], 
+     "PF1":["",0], 
+     "PF2":["",0], 
+     "C":  ["",0] 
+    };
+
+    function updatelineup(pos, name, salary) {
+        if(pos!="C") {
+            pos1 = pos + "1";
+            pos2 = pos + "2";
+            console.log(lineup[pos1]);
+            if(lineup[pos1][0]=="")
+                lineup[pos1] = [name,salary];
+            else
+                lineup[pos2] = [name,salary];
+        }
+        else
+            lineup[pos] = [name,salary];
+
+        console.log(lineup);
+        writelineupdiv();
+    }
+    function writelineupdiv()
+    {
+        var dfslineuptext = "";
+        var totalsalary = 0; 
+        for (var key in lineup){
+            dfslineuptext += key + " " + lineup[key][0] + " " + lineup[key][1] + "</br>" ;
+            totalsalary += parseInt(lineup[key][1]);
+            //console.log( key, dict[key] );
+        }
+        dfsdiv = document.getElementById('dfslineupdiv');
+        dfsdiv.innerHTML = dfslineuptext + "</br>" + totalsalary;
+    }
