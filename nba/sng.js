9c6f8e283a8a63f1cf0ce5ecb9b323de34779b9a
unknown <vmahtani1@gmail.com>
2022-02-08 13:22:35 -0500

    changes

--- nba/sng.js
+++ nba/sng.js
@@ -47,11 +47,11 @@
 
     function getexternal(stattype, teamname, gameid) {
         console.log(stattype, teamname);
-        var teamnameespn = teamname;
+        var teamnameespn = teamname.toUpperCase();
         if(teamnameespn == "NOP")
-            teamnameespn == "NO";
+            teamnameespn = "NO";
         else if(teamnameespn == "UTA")
-            teamnameespn == "UTAH";
+            teamnameespn = "UTAH";
         if(stattype=="DEP")
             url = "https://www.espn.com/nba/team/depth/_/name/" + teamnameespn.toLowerCase();
         else if(stattype=="INJ")
@@ -69,3 +69,51 @@
         imgnode.src = file;
         statsdiv.appendChild(imgnode);
     }
+
+    var lineup={ 
+        "PG1":["",0], 
+        "PG2":["",0], 
+        "SG1":["",0], 
+        "SG2":["",0], 
+        "SF1":["",0], 
+        "SF2":["",0], 
+        "PF1":["",0], 
+        "PF2":["",0], 
+        "C":  ["",0] 
+       };
+   
+       function updatelineup(pos, name, salary) {
+           if(pos!="C") {
+               pos1 = pos + "1";
+               pos2 = pos + "2";
+               console.log(lineup[pos1]);
+               if(lineup[pos1][0]=="")
+                   lineup[pos1] = [name,salary];
+               else
+                   lineup[pos2] = [name,salary];
+           }
+           else
+               lineup[pos] = [name,salary];
+   
+           console.log(lineup);
+           writelineupdiv();
+       }
+       function cleardfskey(key){
+           lineup[key][0] = "";
+           lineup[key][1] = 0;
+           writelineupdiv();
+       }
+       function writelineupdiv()
+       {
+           var dfslineuptext = "";
+           var totalsalary = 0; 
+           for (var key in lineup){
+                spantext = "<span id=\"dfs_x\" onclick=\"cleardfskey('"+ key + "')\")>X</span>"
+                dfslineuptext += "<h3>" + key + " " + lineup[key][0] + " " + lineup[key][1] + " " + spantext + "</h3>" ;
+                totalsalary += parseInt(lineup[key][1]);
+                //console.log( key, dict[key] );
+           }
+           dfsdiv = document.getElementById('dfslineupdiv');
+           dfsdiv.innerHTML = dfslineuptext + "<h3>" + totalsalary + "</h3>";
+       }
+       
